9b2e50077a5e12caf585634473d179d68fd6fb1d
Aashrey Sharma <aashrey9sharma@gmail.com>
2021-10-17 20:12:58 -0400

    Updated onboarding key to be unique to Prototyper

--- src/onboarding.ts
+++ src/onboarding.ts
@@ -1,6 +1,6 @@
 export class Onboarding {
 
-  static ONBOARDING_KEY = 'onboarding'
+  static ONBOARDING_KEY = 'com.aashreys.prototyper.onboarding'
 
   static isCompleteAsync(): Promise<boolean | undefined> {
     return figma.clientStorage.getAsync(Onboarding.ONBOARDING_KEY)
