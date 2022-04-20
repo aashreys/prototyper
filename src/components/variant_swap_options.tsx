1d5e656458863845be95ade8a94761d6ad485203
Aashrey Sharma <aashrey9sharma@gmail.com>
2022-04-19 21:58:29 -0400

    Refactored Variant Swap options to remove redunctant component classes

--- src/components/variant_swap_options.tsx
+++ src/components/variant_swap_options.tsx
@@ -1,65 +1,9 @@
-import { Columns, IconArrowRight16, MiddleAlign, Stack, Text, Textbox, VerticalSpace } from '@create-figma-plugin/ui'
-import { Component, Fragment, h, JSX } from 'preact'
-import { useState } from 'preact/hooks'
+import { Columns, MiddleAlign, Text, Textbox } from '@create-figma-plugin/ui'
+import { Component, h } from 'preact'
 import { AaIcon } from '../icons/aa'
 import { ArrowRightIcon } from '../icons/arrow_right'
 import { DiamondIcon } from '../icons/diamond'
 import { DiamondOutlineIcon } from '../icons/diamond_outline'
-import styles from '../styles.css'
-
-const VariantPropertyTextbox = function (props) {
-  const [value, setValue] = useState(props.value)
-  function handleInput(event: JSX.TargetedEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    setValue(newValue)
-    props.onPropertyChange(newValue);
-  }
-  return (
-    <Textbox 
-    icon={<AaIcon/>}
-    disabled={props.disabled} 
-    onInput={handleInput} 
-    placeholder="Variant Property" 
-    value={value} 
-    noBorder />
-  )
-}
-
-const VariantFromValueTextbox = function (props) {
-  const [value, setValue] = useState(props.value)
-  function handleInput(event: JSX.TargetedEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    setValue(newValue)
-    props.onFromChange(newValue)
-  }
-  return (
-    <Textbox 
-    icon={<DiamondOutlineIcon/>}
-    disabled={props.disabled} 
-    onInput={handleInput} 
-    placeholder="From Variant" 
-    value={value} 
-    noBorder />
-  )
-}
-
-const VariantToValueTextbox = function (props) {
-  const [value, setValue] = useState(props.value)
-  function handleInput(event: JSX.TargetedEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    setValue(newValue)
-    props.onToChange(newValue)
-  }
-  return (
-    <Textbox 
-    icon={<DiamondIcon/>}
-    disabled={props.disabled} 
-    onInput={handleInput} 
-    placeholder="To Variant" 
-    value={value} 
-    noBorder />
-  )
-}
 
 export class VariantSwapOptions extends Component<any, any>  {
 
@@ -69,12 +13,13 @@ export class VariantSwapOptions extends Component<any, any>  {
   }
 
   bindMethods() {
-    this.onPropertyChange = this.onPropertyChange.bind(this);
-    this.onFromChange = this.onFromChange.bind(this);
-    this.onToChange = this.onToChange.bind(this);
+    this.onPropertyChange = this.onPropertyChange.bind(this)
+    this.onFromChange = this.onFromChange.bind(this)
+    this.onToChange = this.onToChange.bind(this)
   }
 
   onPropertyChange(property) {
+    console.log('Property: ' + property)
     this.props.onSwapVariantChange({
       ...this.props.swapVariant,
       property: property
@@ -82,6 +27,7 @@ export class VariantSwapOptions extends Component<any, any>  {
   }
 
   onFromChange(from) {
+    console.log('From: ' + from)
     this.props.onSwapVariantChange({
       ...this.props.swapVariant,
       from: from
@@ -89,6 +35,7 @@ export class VariantSwapOptions extends Component<any, any>  {
   }
 
   onToChange(to) {
+    console.log('To: ' + to)
     this.props.onSwapVariantChange({
       ...this.props.swapVariant,
       to: to
@@ -98,7 +45,7 @@ export class VariantSwapOptions extends Component<any, any>  {
   render(props, state) {
     return (
       <div style={props.style ? props.style : ''}>
-        <Text style={'margin-left: 8px'} muted={props.disabled} bold>Swap Variant</Text>
+        <Text style={'margin-left: 8px'} muted={props.disabled} bold>Move Navigation Focus</Text>
 
         <div style='height: 12px' />
         
@@ -111,11 +58,13 @@ export class VariantSwapOptions extends Component<any, any>  {
             </div>
           }
 
-          <VariantPropertyTextbox
-            disabled={this.props.disabled}
-            onPropertyChange={this.onPropertyChange}
-            value={props.swapVariant.property}
-          />
+          <Textbox 
+          icon={<AaIcon/>}
+          disabled={props.disabled} 
+          onInput={e => this.onPropertyChange(e.currentTarget.value)}
+          placeholder="Variant Property" 
+          value={props.swapVariant.property} 
+          noBorder />
 
           <div style='height: 4px' />
 
@@ -127,21 +76,29 @@ export class VariantSwapOptions extends Component<any, any>  {
           }
 
           <Columns>
-            <VariantFromValueTextbox
-              disabled={props.disabled}
-              onFromChange={this.onFromChange}
-              value={props.swapVariant.from}
-            />
+
+            <Textbox 
+            icon={<DiamondOutlineIcon/>}
+            disabled={props.disabled} 
+            onInput={e => this.onFromChange(e.currentTarget.value)} 
+            placeholder="From Variant" 
+            value={props.swapVariant.from} 
+            noBorder />
+
             <div style='padding-left: 4px; padding-right: 4px'>
               <MiddleAlign>
                 <ArrowRightIcon fill={props.disabled ? "#eaeaea" : "#b0b0b0"} />
               </MiddleAlign>
             </div>
-            <VariantToValueTextbox
-              disabled={props.disabled}
-              onToChange={this.onToChange}
-              value={props.swapVariant.to}
-            />
+
+            <Textbox 
+            icon={<DiamondIcon/>}
+            disabled={props.disabled} 
+            onInput={e => this.onToChange(e.currentTarget.value)}
+            placeholder="To Variant" 
+            value={props.swapVariant.to} 
+            noBorder />
+
           </Columns>
 
         </div>
