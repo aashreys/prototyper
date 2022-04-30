a25caa2a04b8fd0bebbd2a6edc002f25a84b42a8
Aashrey Sharma <aashrey9sharma@gmail.com>
2022-04-30 14:22:59 -0400

    Removed redundant UI components from Animation Options

--- src/components/animation_options.tsx
+++ src/components/animation_options.tsx
@@ -1,5 +1,5 @@
-import { Columns, Dropdown, DropdownOption, TextboxNumeric, VerticalSpace, Text, Inline, Textbox, SegmentedControl, SegmentedControlOption, Checkbox, IconAdjust32, MiddleAlign} from '@create-figma-plugin/ui'
-import { Component, Fragment, h, JSX } from 'preact'
+import { Dropdown, DropdownOption, VerticalSpace, Text, Textbox, SegmentedControl, SegmentedControlOption, Checkbox, MiddleAlign} from '@create-figma-plugin/ui'
+import { Component, h, JSX } from 'preact'
 import { useState } from 'preact/hooks'
 import { AnimationDirection, AnimationEasing, AnimationType } from '../animation'
 import { ArrowDownIcon } from '../icons/arrow_down'
@@ -18,14 +18,6 @@ import { TransitionDissolveIcon } from '../icons/dissolve'
 import { TransitionMoveIcon } from '../icons/move'
 import { TransitionSmartAnimateIcon } from '../icons/smart_animate'
 import { TimerIcon } from '../icons/timer'
-
-/* 
-Animation Type - Instant, Dissolve, Smart Animate, Move In, Move Out, Push, Slide In, Slide Out
-Direction - Left, Right, Up, Down
-Easing - Linear, Ease In, Ease Out, Ease In & Out...
-Duration - time in ms
-*/
-
 
 // Animation Type
 const INSTANT = 'Instant'
@@ -53,122 +45,37 @@ const EASE_IN_BACK = 'Ease in back'
 const EASE_OUT_BACK = 'Ease out back'
 const EASE_IN_AND_OUT_BACK = 'Ease in and out back'
 
-const TypeSelect = function (props) {
+const TRANSITION_OPTIONS: Array<DropdownOption> = [
+  { value: INSTANT },
+  { value: DISSOLVE },
+  { value: SMART_ANIMATE },
+  { separator: true },
+  { value: MOVE_IN },
+  { value: MOVE_OUT },
+  { value: PUSH },
+  { value: SLIDE_IN },
+  { value: SLIDE_OUT },
+]
 
-  const [value, setValue] = useState<null | string>(props.value)
+const DIRECTION_OPTIONS: Array<SegmentedControlOption> = [
+  { value: AUTO },
+  { value: LEFT, children: <ArrowLeftIcon /> },
+  { value: RIGHT, children: <ArrowRightIcon /> },
+  { value: BOTTOM, children: <ArrowDownIcon /> },
+  { value: TOP, children: <ArrowUpIcon /> },
+]
 
-  const options: Array<DropdownOption> = [
-    { value: INSTANT },
-    { value: DISSOLVE },
-    { value: SMART_ANIMATE },
-    { separator: true },
-    { value: MOVE_IN },
-    { value: MOVE_OUT },
-    { value: PUSH },
-    { value: SLIDE_IN },
-    { value: SLIDE_OUT },
-  ]
-
-  function getIcon(value) {
-    switch (value) {
-      case INSTANT: return <TransitionInstantIcon />
-      case DISSOLVE: return <TransitionDissolveIcon />
-      case SMART_ANIMATE: return <TransitionSmartAnimateIcon />
-      default: return <TransitionMoveIcon />
-    }
-  }
-
-  function handleChange(event: JSX.TargetedEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    setValue(newValue)
-    props.callback(newValue)
-  }
-
-  return (
-    <Dropdown
-      icon={getIcon(value)}
-      onChange={handleChange}
-      options={options}
-      value={value}
-      noBorder
-    />
-  )
-
-}
-
-const DirectionSelect = function (props) {
-
-  const [value, setValue] = useState(props.value)
-
-  const options: Array<SegmentedControlOption> = [
-    { value: AUTO },
-    { value: LEFT, children: <ArrowLeftIcon /> },
-    { value: RIGHT, children: <ArrowRightIcon /> },
-    { value: BOTTOM, children: <ArrowDownIcon /> },
-    { value: TOP, children: <ArrowUpIcon /> },
-  ]
-
-  function handleChange(event: JSX.TargetedEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    setValue(newValue)
-    props.callback(newValue);
-  }
-
-  return (
-    <SegmentedControl 
-      onChange={handleChange} 
-      options={options} 
-      value={props.value}
-    />
-  )
-
-}
-
-const EasingSelect = function (props) {
-
-  const [value, setValue] = useState<null | string>(props.value)
-
-  const options: Array<DropdownOption> = [
-    { value: LINEAR },
-    { separator: true },
-    { value: EASE_IN },
-    { value: EASE_OUT },
-    { value: EASE_IN_AND_OUT },
-    { separator: true },
-    { value: EASE_IN_BACK },
-    { value: EASE_OUT_BACK },
-    { value: EASE_IN_AND_OUT_BACK },
-  ]
-
-  function getIcon(value) {
-    switch (value) {
-      case LINEAR: return <CurveLinearIcon />
-      case EASE_IN: return <CurveEaseInIcon />
-      case EASE_OUT: return <CurveEaseOutIcon />
-      case EASE_IN_AND_OUT: return <CurveEaseInOutIcon />
-      case EASE_IN_BACK: return <CurveEaseInBackIcon />
-      case EASE_OUT_BACK: return <CurveEaseOutBackIcon />
-      case EASE_IN_AND_OUT_BACK: return <CurveEaseInOutBackIcon />
-    }
-  }
-
-  function handleChange(event: JSX.TargetedEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.value
-    setValue(newValue)
-    props.callback(newValue);
-  }
-
-  return (
-    <Dropdown
-      icon={getIcon(value)}
-      onChange={handleChange}
-      options={options}
-      value={value}
-      noBorder
-    />
-  )
-
-}
+const EASING_OPTIONS: Array<DropdownOption> = [
+  { value: LINEAR },
+  { separator: true },
+  { value: EASE_IN },
+  { value: EASE_OUT },
+  { value: EASE_IN_AND_OUT },
+  { separator: true },
+  { value: EASE_IN_BACK },
+  { value: EASE_OUT_BACK },
+  { value: EASE_IN_AND_OUT_BACK },
+]
 
 const DurationInput = function (props) {
 
@@ -221,31 +128,12 @@ const DurationInput = function (props) {
 
   return (
     <Textbox
-      icon={<TimerIcon />}
-      validateOnBlur={validateOnBlur}
-      onInput={handleInput}
-      placeholder='Duration'
-      value={value}
-      noBorder
-    />
-  )
-
-}
-
-const MatchLayersSelect = function (props) {
-
-  const [value, setValue] = useState(false)
-
-  function handleChange(event: JSX.TargetedEvent<HTMLInputElement>) {
-    const newValue = event.currentTarget.checked
-    setValue(newValue)
-    props.callback(newValue)
-  }
-
-  return (
-    <Checkbox onChange={handleChange} value={props.value}>
-      <Text>Smart animate matching layers</Text>
-    </Checkbox>
+    icon={<TimerIcon />}
+    validateOnBlur={validateOnBlur}
+    onInput={handleInput}
+    placeholder='Duration'
+    value={value}
+    noBorder />
   )
 
 }
@@ -368,52 +256,82 @@ export class AnimationOptions extends Component<any, any> {
     }
   }
 
+  getIcon(value) {
+    switch (value) {
+      case INSTANT: return <TransitionInstantIcon />
+      case DISSOLVE: return <TransitionDissolveIcon />
+      case SMART_ANIMATE: return <TransitionSmartAnimateIcon />
+      case LINEAR: return <CurveLinearIcon />
+      case EASE_IN: return <CurveEaseInIcon />
+      case EASE_OUT: return <CurveEaseOutIcon />
+      case EASE_IN_AND_OUT: return <CurveEaseInOutIcon />
+      case EASE_IN_BACK: return <CurveEaseInBackIcon />
+      case EASE_OUT_BACK: return <CurveEaseOutBackIcon />
+      case EASE_IN_AND_OUT_BACK: return <CurveEaseInOutBackIcon />
+      default: return <TransitionMoveIcon />
+    }
+  }
+
   render(props, state) {
     return (
       <div style={props.style ? props.style : ''}>
         <Text style={'margin-left: 8px'} bold>Transition</Text>
+        
         <VerticalSpace space='small' />
+        
         <div style='display: flex'>
-          <div style='min-width: 35%; flex-grow: 1'>
-            <TypeSelect
-            callback={this.onTypeChange}
-            value={this.getUiValue(props.animation.type)} />
-          </div>
+
+          <Dropdown // Transition Select
+          style={'min-width: 35%; flex-grow: 1'}
+          icon={this.getIcon(this.getUiValue(props.animation.type))}
+          onChange={e => this.onTypeChange(e.currentTarget.value)}
+          options={TRANSITION_OPTIONS}
+          value={this.getUiValue(props.animation.type)}
+          noBorder />
+
           {
             this.isDirectional() && 
-            <div>
-              <MiddleAlign>
-                <DirectionSelect
-                callback={this.onDirectionChange}
-                value={props.animation.isAutoDirection ? AUTO : 
-                  this.getUiValue(props.animation.direction)} />
-              </MiddleAlign>
-            </div>
+            <div> <MiddleAlign>
+              <SegmentedControl // Direction Select
+              onChange={e => this.onDirectionChange(e.currentTarget.value)} 
+              options={DIRECTION_OPTIONS} 
+              value={props.animation.isAutoDirection ? AUTO : 
+                this.getUiValue(props.animation.direction)} />
+            </MiddleAlign> </div>
           }
         </div>
 
         {
           this.isTimedAndEased() &&
           <div style='display: flex; margin-top: 4px;'>
-            <div style='width: 50%;'> 
-              <EasingSelect 
-              callback={this.onEasingChange} 
-              value={this.getUiValue(props.animation.easing)} /> 
-            </div>
+
+            <Dropdown // Easing Select
+            style={'width: 50%;'}
+            icon={this.getIcon(this.getUiValue(props.animation.easing))}
+            onChange={e => this.onEasingChange(e.currentTarget.value)}
+            options={EASING_OPTIONS}
+            value={this.getUiValue(props.animation.easing)}
+            noBorder />
+
             <div style='width: 50%;'>
-            <DurationInput 
-            callback={this.onDurationChange} 
-            value={props.animation.duration} />
+              <DurationInput // Duration Input
+              callback={this.onDurationChange} 
+              value={props.animation.duration} />
             </div>
+
           </div> 
         }
 
         {
           this.isDirectional() && 
           <div style='margin-top: 12px; margin-bottom: 8px; margin-left: 8px; margin-right: 8px'>
-            <MatchLayersSelect
-            callback={this.onIsMatchLayersChange}
-            value={props.animation.isMatchLayers} />
+
+            <Checkbox // Match Layers Checkbox
+            onChange={e => this.onIsMatchLayersChange(e.currentTarget.checked)} 
+            value={props.animation.isMatchLayers} >
+              <Text>Smart animate matching layers</Text>
+            </Checkbox>
+            
           </div>
         }
         
